09b6ee15eb59b39243df986553ca22b4f5ddf694
VickyFengYu <39153277+VickyFengYu@users.noreply.github.com>
2020-02-26 20:34:10 +0800

    Update Foo.java

--- java/Foo.java
+++ java/Foo.java
@@ -1,13 +1,14 @@
 
 
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-class Foo {
+class Foo_1 {
 
   private AtomicInteger firstJobDone = new AtomicInteger(0);
   private AtomicInteger secondJobDone = new AtomicInteger(0);
 
-  public Foo() {}
+  public Foo_1() {}
 
   public void first(Runnable printFirst) throws InterruptedException {
     // printFirst.run() outputs "first".
@@ -33,4 +34,61 @@ class Foo {
     // printThird.run() outputs "third".
     printThird.run();
   }
+}
+
+class Foo_2 {
+
+  Semaphore run2, run3;
+
+  public Foo_2() {
+    run2 = new Semaphore(0);
+    run3 = new Semaphore(0);
+  }
+
+  Runnable printFirst =
+      new Runnable() {
+        public void run() {
+          System.out.print("first");
+          run2.release();
+        }
+      };
+
+  Runnable printSecond =
+      new Runnable() {
+        public void run() {
+          try {
+            run2.acquire();
+          } catch (InterruptedException e) {
+            System.out.println("Error");
+          }
+          System.out.print("second");
+          run3.release();
+        }
+      };
+
+  Runnable printThird =
+      new Runnable() {
+        public void run() {
+          try {
+            run3.acquire();
+          } catch (InterruptedException e) {
+            System.out.println("Error");
+          }
+          System.out.print("third");
+        }
+      };
+  
+
+  public static void main(String[] args) {
+    Foo_2 bar = new Foo_2();
+
+    Thread one = new Thread(bar.printFirst);
+    Thread two = new Thread(bar.printSecond);
+    Thread three = new Thread(bar.printThird);
+
+    one.start();
+    two.start();
+    three.start();
+  }
+  
 }
